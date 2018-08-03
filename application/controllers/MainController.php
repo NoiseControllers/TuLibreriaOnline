99201522316ea40cbf418925c5538eec36933009
NoiseController <masterboxkm1@gmail.com>
2018-08-03 12:23:11 +0200

    Creating the Parent Controller

---
+++ application/controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jose
+ * Date: 03/08/2018
+ * Time: 12:20
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MainController extends CI_Controller
+{
+
+}
